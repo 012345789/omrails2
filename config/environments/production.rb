07ad615d59b805a59e8812fa5ceaf827ad06309b
Gorlan Yuen <holyduper@gmail.com>
2013-11-22 09:11:19 -0800

    Attempt 2 to fix user sign up confirmation in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,16 +89,13 @@ Omrails::Application.configure do
   #}
   #above commented out for testing
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-     :tls => true,
-     :address => "smtp.gmail.com",
+     :address => 'smtp.gmail.com',
      :port => 587,
-     :domain => "gmail.com",
-     :authentication => :login,
-     :user_name => "personalstatementguru@gmail.com",
-     :password => "superstatement!"
+     :domain => 'gmail.com',
+     :authentication => :plain,
+     :user_name => 'personalstatementguru@gmail.com',
+     :password => 'superstatement!'
    }
 
 
