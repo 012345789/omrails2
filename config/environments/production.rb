b3b93604a03bdad36c7328bb3f3176a7dbf3c748
Gorlan Yuen <holyduper@gmail.com>
2013-11-23 07:53:40 -0800

    Testing user confirmation in production 6.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Omrails::Application.configure do
   config.action_mailer.smtp_settings = {
      :enable_starttls_auto => true,
      :tls => true,
-     :address => 'secure-crag-4919.herokuapp.com',
+     :address => "smtp.gmail.com",
      :port => 587,
      :domain => 'gmail.com',
      :authentication => :login,
