2d5e2c8b8003bfa72396734fcbf0857caf809ed3
Gorlan Yuen <holyduper@gmail.com>
2013-11-24 12:33:52 -0800

    Testing user confirmation in production 17.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # setting the log level
+  config.log_level = :debug
 
   #:host should be set to the actual host (domain name to be registered)
   #config.action_mailer.default_url_options = { :host => 'localhost:3000'}    
